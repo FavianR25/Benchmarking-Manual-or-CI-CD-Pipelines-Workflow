0c60e54d0f5fc970a70618f1abb1672ab1f35ee1
Xhykane <Raxeronorouzi@gmail.com>
2025-11-25 21:56:51 +0700

    Add preprocessing script for CI/CD pipeline
    This script reads a CSV file, fills missing values for 'Age' and 'Fare' with their medians, encodes 'Sex' and 'Embarked' columns, and saves the processed data to a new CSV file.

--- cicd_pipeline/preprocess.py
+++ cicd_pipeline/preprocess.py
@@ -1,1 +1,26 @@
+import pandas as pd
+import os
+
+INPUT_PATH = "train.csv"
+OUTPUT_PATH = "cicd_pipeline/processed_cicd.csv"
+
+def preprocess():
+    print("CI/CD Preprocessing started...")
+
+    df = pd.read_csv(INPUT_PATH)
+
+    df.fillna({
+        "Age": df["Age"].median(),
+        "Fare": df["Fare"].median()
+    }, inplace=True)
+
+    df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
+    df["Embarked"] = df["Embarked"].map({"S": 0, "C": 1, "Q": 2})
+    df["Embarked"].fillna(0, inplace=True)
+
+    df.to_csv(OUTPUT_PATH, index=False)
+    print(f"Preprocessing completed. Saved to {OUTPUT_PATH}")
+
+if __name__ == "__main__":
+    preprocess()
 
